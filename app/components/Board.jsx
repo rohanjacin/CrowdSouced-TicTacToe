46c5be677f3ff61ca94de0b6a08e8f74b62616ad
Rohan Jacinto <rohanjacin@gmail.com>
2024-11-08 13:15:14 +0530

    Added enumeration of cells as per row and col and also as per quadrant for level2
    Signed-off-by: Rohan Jacinto <rohanjacin@gmail.com>

--- app/components/Board.jsx
+++ app/components/Board.jsx
@@ -2,61 +2,83 @@ import React from "react";
 import Cell from "./Cell.jsx";
 import Strike from "./Strike.jsx";
 
-function Board({ quad, cells, onCellClick }) {
+function Board({ level, quad, off, cells, onCellClick }) {
+	let marker = (level == 2)? 9 : 3;
+	
 	return(
 		<div className="board">
 			<Cell 
-				onClick={()=> onCellClick(0)}
-				value={cells[0]}
-				className={getBorder(quad, 0)}/>
+				onClick={()=> onCellClick(off+marker*0+0)}
+				value={cells[off+0]}
+				className={getBorder(level, quad, 0)}/>
 			<Cell 
-				onClick={()=> onCellClick(1)}
-				value={cells[1]}
-				className={getBorder(quad, 1)}/>
+				onClick={()=> onCellClick(off+marker*0+1)}
+				value={cells[off+1]}
+				className={getBorder(level, quad, 1)}/>
 			<Cell 
-				onClick={()=> onCellClick(2)}
-				value={cells[2]}
-				className={getBorder(quad, 2)}/>
+				onClick={()=> onCellClick(off+marker*0+2)}
+				value={cells[off+2]}
+				className={getBorder(level, quad, 2)}/>
 			
 			<Cell 
-				onClick={()=> onCellClick(3)}
-				value={cells[3]}
-				className={getBorder(quad, 3)}/>
+				onClick={()=> onCellClick(off+marker*1+0)}
+				value={cells[off+marker+0]}
+				className={getBorder(level, quad, 3)}/>
 			<Cell 
-				onClick={()=> onCellClick(4)}
-				value={cells[4]}
-				className={getBorder(quad, 4)}/>
+				onClick={()=> onCellClick(off+marker*1+1)}
+				value={cells[off+marker+1]}
+				className={getBorder(level, quad, 4)}/>
 			<Cell 
-				onClick={()=> onCellClick(5)}
-				value={cells[5]}
-				className={getBorder(quad, 5)}/>
+				onClick={()=> onCellClick(off+marker*1+2)}
+				value={cells[off+marker+2]}
+				className={getBorder(level, quad, 5)}/>
 
 			<Cell 
-				onClick={()=> onCellClick(6)}
-				value={cells[6]}
-				className={getBorder(quad, 6)}/>
+				onClick={()=> onCellClick(off+marker*2+0)}
+				value={cells[off+2*marker+0]}
+				className={getBorder(level, quad, 6)}/>
 			<Cell 
-				onClick={()=> onCellClick(7)}
-				value={cells[7]}
-				className={getBorder(quad, 7)}/>
+				onClick={()=> onCellClick(off+marker*2+1)}
+				value={cells[off+2*marker+1]}
+				className={getBorder(level, quad, 7)}/>
 			<Cell 
-				onClick={()=> onCellClick(8)}
-				value={cells[8]}
-				className={getBorder(quad, 8)}/>
+				onClick={()=> onCellClick(off+marker*2+2)}
+				value={cells[off+2*marker+2]}
+				className={getBorder(level, quad, 8)}/>
 
 			<Strike />
 		</div>
 	);
 }
 
-function getBorder(qid, cid) {
+function getBorder(level, qid, cid) {
 
 	switch (qid) {
-		case 0:
+		case 0: {
+			if (level == 2)
+				return "right-border bottom-border";
+			else {
+				switch (cid) {
+					case 0:
+					case 1:
+					case 3:
+					case 4:
+						return "right-border bottom-border";
+					case 2:
+					case 5:
+						return "bottom-border";
+					case 6:
+					case 7:
+						return "right-border";
+					case 8:
+						return "";
+				}
+			}
+		}
 		case 1:
 		case 3:
-		case 4:			
-			return "right-border bottom-border";
+		case 4:
+			return "right-border bottom-border";	
 		break;
 		case 2:
 		case 5: {
--- app/components/Game.jsx
+++ app/components/Game.jsx
@@ -3,65 +3,97 @@ import Board from "./Board.jsx";
 import { useState } from "react";
 
 function Game() {
-	const [cells, setCells] = useState(Array(9).fill(null));
+	// Level
+	const [level, setLevel] = useState(2);
+	// Level cell count
+	const numCells = (level == 2)? 81 : 9;
+	const marker = (level == 2)? 9 : 3;
+	
+	// Cell array formats
+	const [cells, setCells] = useState((level == 2) ? 
+							  (Array.from({ length: 9 }, 
+							  () => new Array(9).fill(null))) :
+							  (Array.from({ length: 3 }, 
+							  () => new Array(3).fill(null))));
+	const [quadCells, setQuadCells] = useState(Array(numCells).fill(null));
 
+	// On move send row and col of cell to Game.sol
 	const handleCellClick = (index) => {
+
+		let row = Math.floor(index/numCells);
+		let col = index%numCells;
+		
 		const newCells = [...cells];
-		newCells[index] = "❌";
-		setCells(newCells); 
+		newCells[row, col] = index;
+		setCells(newCells);
+
+		let idx = row*marker+col;
+		const newQuadCells = [...quadCells];
+		newQuadCells[idx] = index;
+		setQuadCells(newQuadCells);
 	}
 
 	return(
 		<div className="game">
 			<h1>
 				<div>
-				<Board quad={0} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={0} off={0*marker+3*0} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={1} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={1} off={0*marker+3*1} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={2} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={2} off={0*marker+3*2} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={3} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={3} off={3*marker+3*0} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={4} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={4} off={3*marker+3*1} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> 
+				: <div> <Board quad={0} cells={quadCells} 
+				onCellClick={handleCellClick}/> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={5} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={5} off={3*marker+3*2} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={6} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={6} off={6*marker+3*0} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={7} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={7} off={6*marker+3*1} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 			<h1>
 				<div>
-				<Board quad={8} cells={cells} onCellClick={handleCellClick}/>
+				{(level == 2)? <div> <Board level={level} quad={8} off={6*marker+3*2} cells={quadCells} 
+				onCellClick={handleCellClick}/></div> : <div> </div>}	
 				</div>
 			</h1>
 		</div>
 
 	);
 }
-			//<Board cells={cells} onCellClick={handleCellClick}/>
 
 export default Game;
